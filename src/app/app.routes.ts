e05172ade4dc453586aba3e04426e13abe2265a2
Diego-8520 <diego.alejandor.8520@gmail.com>
2026-03-25 21:59:36 -0500

    feat(routing): configurar ruta principal para CharactersPageComponent
    Se crea la primera ruta de la aplicación apuntando al componente CharactersPageComponent.
    
    Cambios realizados:
    - Se importa CharactersPageComponent en app.routes.ts
    - Se define la ruta raíz ('') para renderizar la página de personajes
    - Se integra correctamente con <router-outlet> en app.html
    
    Resultado:
    - La aplicación ahora muestra el componente de personajes al iniciar
    - Se establece la base de navegación para futuras vistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharactersPage } from './features/characters/pages/characters-page/characters-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CharactersPage, //Home
+  },
+];
